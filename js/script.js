1a4425b9d563e6dc70dc5319ca2988279561b004
sepide <sepide@example.com>
2024-07-25 12:52:24 -0400

    update for July25

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("demo2").innerHTML = "JavaScript is fun!";
+
+function myFunction(){
+
+    document.getElementById("demo3").style.fontSize = "40px";
+    document.getElementById("demo3").style.color = "red";
+    document.getElementById("demo3").style.display = "block";
+}
+
+function myFunction2(){
+
+    document.getElementById("demo3").src = "images/background.jpg";
+
+}
+
+function myOver(){
+      //statemnets of the function
+}
