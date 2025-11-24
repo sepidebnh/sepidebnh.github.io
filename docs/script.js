84f1b86760684b116afb4a94577e691bb0b170cb
sepide <sepide@example.com>
2025-11-24 10:19:58 -0500

    update24-nov

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,4 @@
+
 function validateForm(){
 
     let fname = document.forms["frm1"]["fName"].value;
@@ -31,6 +32,55 @@ document.getElementById("test1").innerHTML = numbers;
 
 numbers.sort(function(a, b){return b - a});
 document.getElementById("test1").innerHTML = numbers;
+
+// window.alert("Good Day!");
+
+function moveThis(){
+
+
+let id = null;
+let element = document.getElementById("animated");
+let position = 0;
+clearInterval(id);
+id = setInterval(frame, 5);
+
+function frame(){
+
+    if (position == 350) {
+        clearInterval(id);
+
+
+    }
+
+    else {
+
+        position++;
+        element.style.top = position + "px";
+        element.style.left = position + "px";
+    }
+}     
+}
+
+// create a function 
+function myFunction(arg1, arg2){
+
+    this.firstName = arg1;
+    this.lastName = arg2;
+}
+
+const myObj = new myFunction("John", "Doe");
+
+document.getElementById("test2").innerHTML = myObj.firstName;
+
+
+
+
+
+
+
+
+
+
 
 
 
